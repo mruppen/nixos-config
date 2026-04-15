8d84eec7072b3932372afb32c0ed4a7363988bff
Michael Ruppen <michael.ruppen@pm.me>
2026-04-15 08:47:06 +0200

    chore: remove obsolete code

--- home.nix
+++ home.nix
@@ -12,7 +12,6 @@
   configs = {
     hypr = "hypr";
     nvim = "nvim";
-    # wofi = "wofi";
     rofi = "rofi";
     foot = "foot";
     niri = "niri";
@@ -29,17 +28,6 @@ in {
   home.username = "${username}";
   home.homeDirectory = "/home/${username}";
   home.stateVersion = "25.11";
-  #programs.bash = {
-  #  enable = true;
-  #  shellAliases = {
-  #    btw = "echo i use hyprland btw";
-  #    vim = "nvim";
-  #  };
-  #  initExtra = ''
-  #    export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
-  #    nitch
-  #  '';
-  #};
 
   programs.git = {
     enable = true;
