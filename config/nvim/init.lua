6c0c4d1cab1df04dbfd3aaf94240efe0021f13d0
Michael Ruppen <michael.ruppen@pm.me>
2026-05-08 09:56:55 +0200

    chore(nvim): accept code completion with ctrl - enter

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -768,6 +768,9 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        
+        -- Add Ctrl+Enter to accept the selected suggestion
+        ['<C-CR>'] = { 'accept' },   -- or 'accept_confirm' depending on version  
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
