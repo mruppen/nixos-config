3eaad8d6532f4e23ac1f3f2dfebb2eff1a06d375
Michael Ruppen <michael.ruppen@pm.me>
2026-05-15 20:36:14 +0200

    feat(configuration): set default clock rate

--- configuration.nix
+++ configuration.nix
@@ -52,6 +52,17 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+		
+    # Configure clock settings
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.allowed-rates" = [ 44100 48000 96000 ];
+        "default.clock.quantum" = 64;
+        "default.clock.min-quantum" = 32;
+        "default.clock.max-quantum" = 2048;
+      };
+    };
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
