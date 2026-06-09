ffffb8b46ee187051f3e41231b53cf2e0aa7129a
Michael Ruppen <michael.ruppen@pm.me>
2026-06-09 23:01:10 +0200

    fix(roslyn-ls)

--- modules/roslyn-ls/roslyn-language-server.nix
+++ modules/roslyn-ls/roslyn-language-server.nix
@@ -9,7 +9,7 @@ buildDotnetGlobalTool rec {
 
   # Use the runtime-specific package for Linux
   nugetName = "roslyn-language-server.linux-x64";
-  nugetSha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # ← Will be replaced after first build
+  nugetSha256 = "sha256-33J9AuULIcapeJsUphecOPxgpsTqDLOnvyZWMUUfqyk=";
 
   dotnet-sdk = dotnetCorePackages.sdk_10_0;   # Recommended: .NET 10+
   dotnet-runtime = dotnetCorePackages.runtime_10_0;
